a14e4abb63723b6903a27c4062d1f4fe2048f3a0
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-26 18:36:56 +0200

    create icons

---
+++ src/icon/index.tsx
@@ -0,0 +1,5 @@
+import { ReactComponent as Add } from "./add.svg";
+import { ReactComponent as Edit } from "./edit.svg";
+import { ReactComponent as Home } from "./home.svg";
+
+export { Add, Edit, Home };
