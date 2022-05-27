56d3e8eaa58e8418ef9c5d3131989b74a25c611d
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-27 22:00:23 +0200

    export all components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import ActionButton from "./ActionButton";
+import { AddButton, EditButton, HeaderAddButton, UpdateButton } from "./Buttons";
+import ProductItem from "./ProductItem";
+import ProductList from "./ProductList";
+import SearchInput from "./SearchInput";
+
+export { ActionButton, HeaderAddButton, AddButton, UpdateButton, EditButton, ProductItem, ProductList, SearchInput };
