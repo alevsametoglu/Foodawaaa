8b7aa61bd459a4e5ede45e6bfee9ed9e3d0a3373
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-29 20:04:08 +0200

    navigate product detail when click on product item

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
 import { Product } from "../models";
+import { ROUTER_PATHS } from "../routing/paths";
 import "./ProductItem.scss";
 
 type Props = { product: Product };
 const ProductItem = ({ product }: Props) => {
   return (
-    <div className="product-card">
+    <Link to={ROUTER_PATHS.ProductDetailPage.getUrl(product._id)} className="product-card">
       <img className="img" src={product.image} alt="" />
       <div className="infos">
         <span className="name">{product.name}</span>
         <span className="price">{product.price} &euro;</span>
       </div>
       <div className="description">{product.description}</div>
-    </div>
+    </Link>
   );
 };
 
