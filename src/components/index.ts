4107ab635df62001c5e14cb88864ec47dbb1a061
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-29 20:12:54 +0200

    export new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,22 @@
 import ActionButton from "./ActionButton";
 import { AddButton, EditButton, HeaderAddButton, UpdateButton } from "./Buttons";
+import FormControl from "./FormControl/FormControl";
+import ProductForm from "./ProductForm";
 import ProductItem from "./ProductItem";
 import ProductList from "./ProductList";
 import SearchInput from "./SearchInput";
+import TagList from "./TagList";
 
-export { ActionButton, HeaderAddButton, AddButton, UpdateButton, EditButton, ProductItem, ProductList, SearchInput };
+export {
+  ActionButton,
+  HeaderAddButton,
+  TagList,
+  AddButton,
+  UpdateButton,
+  EditButton,
+  ProductItem,
+  ProductList,
+  SearchInput,
+  ProductForm,
+  FormControl,
+};
