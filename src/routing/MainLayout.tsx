0c173ce96e242e194bd62908f6deae2ab5e42c0b
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-29 20:08:16 +0200

    navigate home-page when click on header-logo

--- src/routing/MainLayout.tsx
+++ src/routing/MainLayout.tsx
@@ -2,6 +2,8 @@ import { ReactComponent as Logo } from "./Logo.svg";
 import { Outlet } from "react-router";
 import { HeaderAddButton } from "../components";
 import { Home } from "../icon";
+import { Link } from "react-router-dom";
+import { ROUTER_PATHS } from "./paths";
 
 const MainLayout = () => {
   return (
@@ -11,17 +13,17 @@ const MainLayout = () => {
           background: "white",
           boxShadow: "0px 5px 24px 0px #41434E1A",
           height: 96,
-          width: "100vw",
+          width: "100%",
         }}
       >
         <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", height: "100%" }}>
-          <div style={{ display: "flex", alignItems: "center", gap: 80 }}>
+          <Link to={ROUTER_PATHS.HomePage.getUrl()} style={{ display: "flex", alignItems: "center", gap: 80 }}>
             <Logo />
-            <span>
+            <div>
               <Home /> Catalogue Produits
-            </span>
-          </div>
-          <HeaderAddButton />
+            </div>
+          </Link>
+          <HeaderAddButton to={ROUTER_PATHS.ProductCreatePage.getUrl()} />
         </div>
       </header>
       <main style={{ maxWidth: 1440, marginTop: 40 }}>
