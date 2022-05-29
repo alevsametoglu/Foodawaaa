788f3c94d626a6d9f50ae3999376f18e9f5d4cef
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-29 20:01:12 +0200

    close icon

--- src/icon/index.tsx
+++ src/icon/index.tsx
@@ -1,5 +1,9 @@
+import { CSSProperties } from "react";
 import { ReactComponent as Add } from "./add.svg";
 import { ReactComponent as Edit } from "./edit.svg";
 import { ReactComponent as Home } from "./home.svg";
 
-export { Add, Edit, Home };
+type Props = { style?: CSSProperties };
+const Close = ({ style }: Props) => <i style={style} className="fa-solid fa-xmark"></i>;
+
+export { Add, Edit, Home, Close };
