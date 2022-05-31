3be544bb3fb30af26e75d23b6de70fe8d483eba9
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-31 12:33:40 +0200

    export new icons

--- src/icon/index.tsx
+++ src/icon/index.tsx
@@ -3,7 +3,9 @@ import { ReactComponent as Add } from "./add.svg";
 import { ReactComponent as Edit } from "./edit.svg";
 import { ReactComponent as Home } from "./home.svg";
 
-type Props = { style?: CSSProperties };
-const Close = ({ style }: Props) => <i style={style} className="fa-solid fa-xmark"></i>;
+type Props = { style?: CSSProperties; className?: string };
+const Close = (props: Props) => <i {...props} className={`fa-solid fa-xmark ${props.className}`}></i>;
+const Rotate = (props: Props) => <i {...props} className={`fa-solid fa-arrow-rotate-left ${props.className}`}></i>;
+const Spinner = (props: Props) => <i {...props} className={`fas fa-spinner fa-spin ${props.className}`}></i>;
 
-export { Add, Edit, Home, Close };
+export { Add, Edit, Home, Close, Rotate, Spinner };
