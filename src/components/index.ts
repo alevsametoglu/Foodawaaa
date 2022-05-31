fedf75250f2b917716d6cdf05f5843255ded09e8
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-31 12:33:27 +0200

    export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,12 @@
 import ActionButton from "./ActionButton";
 import { AddButton, EditButton, HeaderAddButton, UpdateButton } from "./Buttons";
+import Center from "./Center";
 import FormControl from "./FormControl/FormControl";
+import LoaderSpinner from "./LoaderSpinner";
 import ProductForm, { ProductFormType } from "./ProductForm";
 import ProductItem from "./ProductItem";
 import ProductList from "./ProductList";
+import ProductNotFound from "./ProductNotFound/ProductNotFound";
 import SearchInput from "./SearchInput";
 import TagList from "./TagList";
 
@@ -19,6 +22,8 @@ export {
   SearchInput,
   ProductForm,
   FormControl,
+  ProductNotFound,
+  LoaderSpinner,
+  Center,
 };
-
 export type { ProductFormType };
