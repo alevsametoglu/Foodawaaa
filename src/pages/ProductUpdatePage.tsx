be22837b3be792154c1dacfbaa16b51496b68c01
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-05-31 12:37:12 +0200

    update-product api entegration

--- src/pages/ProductUpdatePage.tsx
+++ src/pages/ProductUpdatePage.tsx
@@ -1,29 +1,39 @@
-import { ProductForm, ProductFormType } from "../components";
-
-const sampleProduct = {
-  _id: "6248263c315cbb0021cef887",
-  tags: ["burger", "vegan", "sans gluten"],
-  name: "Maous burgerttt",
-  description: "Gros burger ! Truc de fou ! Il est maous !!",
-  image:
-    "https://cac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcac.2F2018.2F09.2F25.2F03ab5e89-bad7-4a44-b952-b30c68934215.2Ejpeg/850x478/quality/80/crop-from/center/burger-maison.jpeg",
-  price: 20,
-  createdAt: "2022-04-02T10:32:28.334Z",
-  updatedAt: "2022-05-07T14:20:11.227Z",
-  __v: 0,
-};
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { API } from "../api";
+import { Center, ProductForm, ProductFormType, ProductNotFound } from "../components";
+import { Product } from "../models";
+import { appActions } from "../Redux/appSlice";
+import { ROUTER_PATHS } from "../routing/paths";
 
 const ProductUpdatePage = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [product, setProduct] = useState<Product | null>();
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    console.log(params);
+
+    if (params.productId) {
+      dispatch(appActions.showLoading());
+      API.getProduct(params.productId)
+        .then(setProduct)
+        .catch(setErrorMessage)
+        .finally(() => dispatch(appActions.hideLoading()));
+    }
+  }, []);
+
   const updateProduct = (data: ProductFormType) => {
     console.log("update product", data);
-    //TODO: update product on server
+    dispatch(appActions.showLoading());
+    if (!!product) API.updateProduct(product._id, data).then(() => navigate(ROUTER_PATHS.ProductDetailPage.getUrl(product._id)));
   };
 
-  return (
-    <div>
-      <ProductForm product={sampleProduct} onSubmit={updateProduct} />
-    </div>
-  );
+  return <Center>{errorMessage ? <ProductNotFound /> : product && <ProductForm product={product} onSubmit={updateProduct} />}</Center>;
 };
 
 export default ProductUpdatePage;
